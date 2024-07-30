a79cc712ab484792162179a55ff16a10f470f8c3
don k joseph <donkjoseph8@gmail.com>
2024-07-30 16:09:29 +0530

    First Commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+document.addEventListener("DOMContentLoaded", function() {
+    const shopNowBtn = document.getElementById("shopNowBtn");
+    
+    shopNowBtn.addEventListener("click", function() {
+        window.location.href = "#products";
+    });
+});
